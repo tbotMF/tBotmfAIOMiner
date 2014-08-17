32bb412d5b1b06a1f0c23570b1ba0b657f938458
tbotMF <modfrank12@gmail.com>
2014-08-17 15:48:31 -0400

    Update WalkToMine.java

--- actions/WalkToMine.java
+++ actions/WalkToMine.java
@@ -3,7 +3,6 @@ package scripts.actions;
 import org.tribot.api.DynamicClicking;
 import org.tribot.api.General;
 import org.tribot.api.Timing;
-import org.tribot.api.input.Mouse;
 import org.tribot.api.types.generic.Condition;
 import org.tribot.api2007.ChooseOption;
 import org.tribot.api2007.Objects;
@@ -20,6 +19,40 @@ import scripts.sbf.action.Action;
 public final class WalkToMine extends Action {
 	private final RSTile skillTile = skillManager.getSkillTile();
 	private final String primaryZPlaneAction = "Climb-down";
+
+	private void climbDownStairs() {
+		final int z = Player.getPosition().getPlane();
+		final RSObject[] stairs = Objects.find(20,
+				Filters.Objects.actionsContains(primaryZPlaneAction));
+		if (stairs.length > 0 && stairs[0] != null) {
+			if (!stairs[0].isOnScreen())
+				if (PathFinding.aStarWalk(stairs[0].getPosition()))
+					Timing.waitCondition(new Condition() {
+
+						@Override
+						public boolean active() {
+							General.sleep(100, 200);
+							return stairs[0].isOnScreen();
+						}
+
+					}, General.random(4000, 5000));
+
+			if (stairs[0].isOnScreen()
+					&& DynamicClicking.clickRSObject(stairs[0], 3))
+				if (ChooseOption.isOptionValid(primaryZPlaneAction)
+						&& ChooseOption.select(primaryZPlaneAction))
+					Timing.waitCondition(new Condition() {
+
+						@Override
+						public boolean active() {
+							General.sleep(100, 200);
+							return Player.getPosition().getPlane() < z;
+						}
+
+					}, General.random(2000, 3000));
+
+		}
+	}
 
 	@Override
 	public void execute() {
@@ -38,38 +71,7 @@ public final class WalkToMine extends Action {
 					return;
 
 		} else {
-			final int z = Player.getPosition().getPlane();
-			final RSObject[] stairs = Objects.find(20,
-					Filters.Objects.actionsContains(primaryZPlaneAction));
-			if (stairs.length > 0 && stairs[0] != null) {
-				if (!stairs[0].isOnScreen())
-					if (PathFinding.aStarWalk(stairs[0].getPosition()))
-						Timing.waitCondition(new Condition() {
-
-							@Override
-							public boolean active() {
-								General.sleep(100, 200);
-								return stairs[0].isOnScreen();
-							}
-
-						}, General.random(4000, 5000));
-			
-				if (stairs[0].isOnScreen()
-						&& DynamicClicking.clickRSObject(stairs[0], 3))
-					if (ChooseOption.isOptionValid(primaryZPlaneAction)
-							&& ChooseOption.select(primaryZPlaneAction))
-						Timing.waitCondition(new Condition() {
-
-							@Override
-							public boolean active() {
-								General.sleep(100, 200);
-								return Player.getPosition().getPlane() < z;
-							}
-
-						}, General.random(2000, 3000));
-
-			}
-
+			climbDownStairs();
 		}
 
 	}
